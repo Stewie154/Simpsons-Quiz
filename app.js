982238496c9da7181983168c51b046755bd4ad8d
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2020-09-12 17:04:36 +0100

    version 2
    I've changed the layout of the quiz to only show one question at a time as suggested. The quiz seems to function well, but if you see any bugs that I've missed it would be useful! I haven't managed to incorperate every suggestion just yet. But maybe now would be a good time for another assesment for further improvements.
    
    I'll hold off on looking at your quiz for now Dan. Maybe it would be best for you to take another look at this version before I do, so as not to get all the answers before trying to figure it out first.
    
    Anyway, whenever you get a chance mate, thanks again for all the help!

--- app.js
+++ app.js
@@ -1,89 +1,252 @@
 let score = 0;
-const answer1 = document.getElementById('correct1');
-const answer2 = document.getElementById('correct2');
-const answer3 = document.getElementById('correct3');
-const question1 = document.getElementById('question1');
-const question2 = document.getElementById('question2');
-const question3 = document.getElementById('question3');
-const question1Btns = document.getElementById('question1Btns');
-const question2Btns = document.getElementById('question2Btns');
-const question3Btns = document.getElementById('question3Btns');
+const questionContainer = document.getElementById('question-container');
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const question = document.getElementById('question');
+const answerBtns = document.getElementById('answer-btns');
+const showResultsBtn = document.getElementById('show-results');
+const playAgainBtn = document.getElementById('play-again-btn');
+let answer1 = document.getElementById('answer1');
+let answer2 = document.getElementById('answer2');
+let answer3 = document.getElementById('answer3');
+let answer4 = document.getElementById('answer4');
+let currentQuestionIndex 
+
 const scoreContainer = document.getElementById('score-container');
-var scoreDisplay = document.getElementById('score-display');
-var finalMessage = document.getElementById('message');
-var finalPicture = document.getElementById('final-picture');
+let scoreDisplay = document.getElementById('score-display');
+let finalPicture = document.getElementById('final-picture');
+let message = document.getElementById('message');
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', setNextQuestion);
+showResultsBtn.addEventListener('click', showResults);
+playAgainBtn.addEventListener('click', startGame);
 
 
+function startGame(){
+    scoreContainer.classList.add('hide');
+    questionContainer.classList.remove('hide');
+    answerBtns.classList.remove('hide');
+    question.classList.remove('hide');
+    startButton.classList.add('hide');
+    currentQuestionIndex = 0;
+    score = 0;
+    setNextQuestion();
+}
 
+function setNextQuestion(){
+    answer1.addEventListener('click', selectAnswer1);
+    answer2.addEventListener('click', selectAnswer2);
+    answer3.addEventListener('click', selectAnswer3);
+    answer4.addEventListener('click', selectAnswer4);
+    if(currentQuestionIndex === questions.length){
+        showResults();
+    } else{
+        resetState();
+    question.innerHTML = questions[currentQuestionIndex].question;
+    answer1.innerHTML = questions[currentQuestionIndex].answers[0].text;
+    answer2.innerHTML = questions[currentQuestionIndex].answers[1].text;
+    answer3.innerHTML = questions[currentQuestionIndex].answers[2].text;
+    answer4.innerHTML = questions[currentQuestionIndex].answers[3].text; 
+    } 
+}
 
-question1Btns.addEventListener('click', (checkFirstAnswer));
-question2Btns.addEventListener('click', (checkSecondAnswer));
-question3Btns.addEventListener('click', (checkThirdAnswer));
+function resetState(){
+    questionContainer.classList.remove('correct');
+    questionContainer.classList.remove('incorrect');
+    answer1.classList.remove('correct');
+    answer1.classList.remove('incorrect');
+    answer2.classList.remove('correct');
+    answer2.classList.remove('incorrect');
+    answer3.classList.remove('correct');
+    answer3.classList.remove('incorrect');
+    answer4.classList.remove('correct');
+    answer4.classList.remove('incorrect');
+    nextButton.classList.add('hide');
+    showResultsBtn.classList.add('hide');
+}
 
-function checkFirstAnswer(click){
-    buttonClicked = click.target;
-    if (buttonClicked === answer1){
+function selectAnswer1(){
+    answer1.removeEventListener('click', selectAnswer1);
+    answer2.removeEventListener('click', selectAnswer2);
+    answer3.removeEventListener('click', selectAnswer3);
+    answer4.removeEventListener('click', selectAnswer4);
+    if(questions[currentQuestionIndex].answers[0].correct === true){
         answer1.classList.add('correct');
-        question1.style.backgroundColor = 'green';
-        score++;
+        questionContainer.classList.add('correct');
+        score ++;
+        currentQuestionIndex ++;
+        if (currentQuestionIndex === questions.length){
+            showResultsBtn.classList.remove('hide');
+        } else {
+            nextButton.classList.remove('hide');
+        }
     } else {
-        buttonClicked.classList.add('incorrect');
-        answer1.classList.add('correct');
-        question1.style.backgroundColor = 'red';
+        answer1.classList.add('incorrect');
+        questionContainer.classList.add('incorrect');
+        currentQuestionIndex ++;
+        if (currentQuestionIndex === questions.length){
+            showResultsBtn.classList.remove('hide');
+        } else {
+            nextButton.classList.remove('hide');
+        }
     }
-    question1Btns.removeEventListener('click', (checkFirstAnswer));
-}
-   
-function checkSecondAnswer(click){
-    buttonClicked = click.target;
-    if (buttonClicked === answer2){
-        answer2.classList.add('correct');
-        question2.style.backgroundColor = 'green';
-        score++;
-    } else {
-        buttonClicked.classList.add('incorrect');
-        answer2.classList.add('correct');
-        question2.style.backgroundColor = 'red';
-    }
-    question2Btns.removeEventListener('click', (checkFirstAnswer));
-}
+ }
 
-function checkThirdAnswer(click){
-    buttonClicked = click.target;
-    if (buttonClicked === answer3){
-        answer3.classList.add('correct');
-        question3.style.backgroundColor = 'green';
-        score++;
+ function selectAnswer2(){
+    answer1.removeEventListener('click', selectAnswer1);
+    answer2.removeEventListener('click', selectAnswer2);
+    answer3.removeEventListener('click', selectAnswer3);
+    answer4.removeEventListener('click', selectAnswer4);
+    if(questions[currentQuestionIndex].answers[1].correct === true){
+        answer2.classList.add('correct');
+        questionContainer.classList.add('correct');
+        score ++;
+        currentQuestionIndex ++;
+        if (currentQuestionIndex === questions.length){
+            showResultsBtn.classList.remove('hide');
+        } else {
+            nextButton.classList.remove('hide');
+        }
     } else {
-        buttonClicked.classList.add('incorrect');
-        answer3.classList.add('correct');
-        question3.style.backgroundColor = 'red';
+        answer2.classList.add('incorrect');
+        questionContainer.classList.add('incorrect');
+        currentQuestionIndex ++;
+        if (currentQuestionIndex === questions.length){
+            showResultsBtn.classList.remove('hide');
+        } else {
+            nextButton.classList.remove('hide');
+        }
     }
-    question3Btns.removeEventListener('click', (checkThirdAnswer));
-    revealResults();
-}
+ }
 
-function revealResults(){
+ function selectAnswer3(){
+    answer1.removeEventListener('click', selectAnswer1);
+    answer2.removeEventListener('click', selectAnswer2);
+    answer3.removeEventListener('click', selectAnswer3);
+    answer4.removeEventListener('click', selectAnswer4);
+    if(questions[currentQuestionIndex].answers[2].correct === true){
+        answer3.classList.add('correct');
+        questionContainer.classList.add('correct');
+        score ++;
+        currentQuestionIndex ++;
+        if (currentQuestionIndex === questions.length){
+            showResultsBtn.classList.remove('hide');
+        } else {
+            nextButton.classList.remove('hide');
+        }
+    } else {
+        answer3.classList.add('incorrect');
+        questionContainer.classList.add('incorrect');
+        currentQuestionIndex ++;
+        if (currentQuestionIndex === questions.length){
+            showResultsBtn.classList.remove('hide');
+        } else {
+            nextButton.classList.remove('hide');
+        }
+    }
+ }
+
+ function selectAnswer4(){
+    answer1.removeEventListener('click', selectAnswer1);
+    answer2.removeEventListener('click', selectAnswer2);
+    answer3.removeEventListener('click', selectAnswer3);
+    answer4.removeEventListener('click', selectAnswer4);
+    if(questions[currentQuestionIndex].answers[3].correct === true){
+        answer4.classList.add('correct');
+        questionContainer.classList.add('correct');
+        score ++;
+        currentQuestionIndex ++;
+        if (currentQuestionIndex === questions.length){
+            showResultsBtn.classList.remove('hide');
+        } else {
+            nextButton.classList.remove('hide');
+        }
+    } else {
+        answer4.classList.add('incorrect');
+        questionContainer.classList.add('incorrect');
+        currentQuestionIndex ++;
+        if (currentQuestionIndex === questions.length){
+            showResultsBtn.classList.remove('hide');
+        } else {
+            nextButton.classList.remove('hide');
+        }
+    }
+ }
+
+ function showResults(){
+    questionContainer.classList.add('hide');
     scoreContainer.classList.remove('hide');
-    if(score === 3){
-        scoreDisplay.innerHTML = 'You scored 3/3';
-    } else if (score === 2){
-        scoreDisplay.innerHTML = 'You scored 2/3';
-        finalMessage.innerHTML = 'Good score!';
-        finalPicture.src = './resources/images/average-homer.jpg';
-    } else if (score === 1){
-        scoreDisplay.innerHTML = 'You scored 1/3';
-        finalMessage.innerHTML = 'Better than nothing, I suppose!';
-        finalPicture.src = './resources/images/homer-confused.gif';
+    scoreDisplay.innerHTML = `You scored ${score}/${questions.length}!`;
+    if (score <= (questions.length * 0.2)){
+        finalPicture.src = "./resources/images/homer-scared.jpeg";
+        message.innerHTML = "It's the taking part that counts!";
+    } else if (score > (questions.length * 0.2) && score <= (questions.length * 0.4)){
+        finalPicture.src = "./resources/images/homer-confused.gif";
+        message.innerHTML = "I've seen worse!";
+    } else if (score > (questions.length * 0.4) && score < (questions.length * 0.8)){
+        finalPicture.src = "./resources/images/average-homer.jpg";
+        message.innerHTML = "Not bad!";
+    } else if (score >= (questions.length * 0.8) && score < (questions.length)){
+        finalPicture.src = "./resources/images/positive-homer.jpg";
+        message.innerHTML = "You're a true fan!";
     } else {
-        scoreDisplay.innerHTML = 'You scored 0/3';
-        finalMessage.innerHTML = "It's the taking part that counts!";
-        finalPicture.src = './resources/images/homer-scared.jpeg';
+        finalPicture.src ="./resources/images/homer-dance.gif";
+        message.innerHTML = "Perfect Score!";
+
     }
+    playAgainBtn.classList.remove('hide');
+
     
-}
+
+ }
 
 
-
-
+questions = [
+    {
+        question: 'What colour is the skin of the Simpsons family?',
+        answers: [
+            {text: 'yellow', correct: true},
+            {text: 'blue', correct: false},
+            {text: 'green', correct: false},
+            {text: 'purple', correct: false}
+        ]
+    }, 
+    {
+        question: 'What does Bart often tell people to eat?',
+        answers: [
+            {text: 'His shirt', correct: false},
+            {text: 'His hat', correct: false},
+            {text: 'His shorts', correct: true},
+            {text: 'His shoes', correct: false}
+        ]
+    }, 
+    {
+        question: "Who is the owner of 'The Kwick-E-Mart'?",
+        answers: [
+            {text: 'Milhouse', correct: false},
+            {text: 'Nelson', correct: false},
+            {text: 'Homer', correct: false},
+            {text: 'Apu', correct: true}
+        ]
+    }, 
+    {
+        question: 'What is the name of the Principal of Springfield Elementary School?', 
+        answers: [
+            {text: 'Principal Wiggum', correct: false},
+            {text: 'Principal Skinner', correct: true},
+            {text: 'Principal Simpson', correct: false},
+            {text: 'Principal Milhouse', correct: false}
+        ]
+    },
+    {
+        question: 'What is the name of the popular beer in Springfield?',
+        answers: [
+            {text: 'Duff', correct: true},
+            {text: 'Fluff', correct: false},
+            {text: 'Fudd', correct: false},
+            {text: 'Stella', correct: false}
+        ]
+    }
+]
 
